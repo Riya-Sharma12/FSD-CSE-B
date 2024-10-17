b27d49333cf23ce5c66da1ecb1b035150e6a4454
Riya-Sharma12 <sharmariya0163@gmaol.com>
2024-10-17 11:43:09 +0530

    js work commited

---
+++ JSWORK/index.js
@@ -0,0 +1,133 @@
+// console.log("hello world");
+// var a=10;
+// console.log(a);
+// {
+//     var a=20;
+//     console.log(a)
+// }
+// function print(){
+//     var a=30;
+//     console.log(a);
+// }
+// print();
+
+// type of 
+
+// a=10;
+// b="riya sharma"
+// console.log(typeof a);  //number
+// console.log(typeof b);  //string
+// c=Date();
+// console.log(typeof c)  //string
+// console.log(c);
+
+
+//******************************************************************************************* */
+
+
+// let a=10;
+// let b="10"
+// if(a==b){
+//     console.log("welcome");
+// }
+// else{
+//     console.log("world");
+// }
+
+//== checks value
+//=== checks type
+
+// let myname= "riya sharma";
+// let myage=123;
+// let result=`Hello ! my name is ${myname} and my age is ${myage}`
+// console.log(result);
+
+
+//working with objects::
+
+
+// let ob ={
+//     name : "riya sharma",
+//     branch : "cse-c",
+//     college : "abes-ec"
+// };
+// console.log(typeof ob);
+// console.log(ob)
+// console.log(ob.name+" branch is: "+ ob.branch+ " and college is:"+ob.college);
+
+//destructuring of the given object:
+
+// const{name}=ob;
+// console.log(name);
+// ob[name]="khushi";
+// console.log(ob.name);
+
+
+// const arr=[
+// {
+//     name: "riya sharma",
+//     class :"cse-b"
+// },
+// {
+//     name: "khushi sharma",
+//     class :"cse-c"
+// },
+// {
+//     name: "shubh sharma",
+//     class :"cse-a"
+// }]
+
+// console.log(arr[1].name);
+
+
+// working with functions::
+
+// function printname(name){
+//     console.log(`hello sherry! my name is ${name}`);
+// }
+// printname("riya sharma")
+
+// function print(msg)
+// {
+//     console.log(msg); // undefined
+// }
+// print();
+
+
+// function add(a,b){
+//     return a+b;
+// }
+// let ans=add(2,3);
+// console.log(ans);
+
+// function sum(a,b,c=9){
+//     return a+b+c;
+// }
+// console.log(sum(1,4));
+
+
+// callback function
+
+// function cCompiler(){
+//     return "cCompler selected";
+// }
+
+// function javaCompiler(){
+//       return "javaCompiler selected";
+// }
+
+// function selectLanguage(click){
+//     console.log("you have "+click);
+// }
+
+// selectLanguage(javaCompiler())  //you have javacompiler selected
+// selectLanguage(javaCompiler)  
+// //  you have function javaCompiler(){
+// //     return "javaCompiler selected";
+// // }
+
+
+
+// dom - 
+
+console.log("hello world");  
